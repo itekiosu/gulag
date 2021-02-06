913d5b9e8cffba73f7fe284d4d47727bf5eaf0f7
Iteki <contact@iteki.pw>
2021-02-06 14:12:08 +0000

    push ver

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if TYPE_CHECKING:
 __all__ = ()
 
 # current version of gulag
-glob.version = cmyui.Version(3, 2, 6)
+glob.version = cmyui.Version(3, 2, 7)
 
 async def on_start() -> None:
     glob.http = aiohttp.ClientSession(json_serialize=orjson.dumps)
