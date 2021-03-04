bf502edf2097981435083a966c10db5daa1e5506
Iteki <contact@iteki.pw>
2021-03-04 15:14:50 +0000

    fix update loop

--- objects/glob.py
+++ objects/glob.py
@@ -57,14 +57,6 @@ cache = {
     # algorithms like brypt these are intentionally designed to be
     # slow; we'll cache the results to speed up subsequent logins.
     'bcrypt': {}, # {bcrypt: md5, ...}
-    # we'll cache results for osu! client update requests since they
-    # are relatively frequently and won't change very frequently.
-    'update': { # default timeout is 1h, set on request.
-        'cuttingedge': {'check': None, 'path': None, 'timeout': 0},
-        'stable40': {'check': None, 'path': None, 'timeout': 0},
-        'beta40': {'check': None, 'path': None, 'timeout': 0},
-        'stable': {'check': None, 'path': None, 'timeout': 0}
-    },
     # cache all beatmap data calculated while online. this way,
     # the most requested maps will inevitably always end up cached.
     'beatmap': {}, # {md5: {timeout, map}, ...}
